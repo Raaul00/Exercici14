04f02a2720664c3f82c0db4a19f854577283167d
Raul Gabriel <raul.stoianof@cirvianum.cat>
2024-09-17 11:47:06 +0200

    Primer commit

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pàgina Nginx PHP Modificada</title>
+</head>
+<body>
+    <h1>Benvingut a la pàgina PHP servida per Nginx!</h1>
+    <p>Aquesta és una aplicació PHP desplegada amb Nginx, modificada per a l'exercici.</p>
+</body>
+</html>
+
